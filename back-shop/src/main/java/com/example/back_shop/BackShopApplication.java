dfac1ad8ae148d0b1cae47c769bbd2a379252075
김현수 <109796155+khstony@users.noreply.github.com>
2026-01-26 18:28:01 +0900

    백엔드 기본파일 업로드

---
+++ back-shop/src/main/java/com/example/back_shop/BackShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackShopApplication.class, args);
+	}
+
+}
---
+++ back-shop/src/test/java/com/example/back_shop/BackShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
